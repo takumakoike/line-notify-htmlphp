dd5a8748f7c970aeaca31a5e9fd3998a3932c43c
takumakoike <takuma.151e@gmail.com>
2024-02-16 00:39:28 +0900

    php edit

--- index.php
+++ index.php
@@ -1,42 +1,42 @@
 <?php
   $code = $_GET['code']; // code
+  var_dump($code)
+  // $data = array(
+  //   "grant_type" => "authorization_code",
+  //   "code" => $code,
+  //   "redirect_uri" => "https://uperworld.com/line/cb.php",
+  //   "client_id" => "bJSIyjA8WGs8wyHBX76bVe",
+  //   "client_secret" => "0M9IkdBdzw4f8DpBExgfqm2aqzx1ja1aQbjesVbfwwA",
+  //   "state" => "aaa"
+  // );
+  // $data = http_build_query($data, "", "&");
 
-  $data = array(
-    "grant_type" => "authorization_code",
-    "code" => $code,
-    "redirect_uri" => "https://uperworld.com/line/cb.php",
-    "client_id" => "bJSIyjA8WGs8wyHBX76bVe",
-    "client_secret" => "0M9IkdBdzw4f8DpBExgfqm2aqzx1ja1aQbjesVbfwwA",
-    "state" => "aaa"
-  );
-  $data = http_build_query($data, "", "&");
+  // $header = array(
+  //   "Content-Type:application/x-www-form-urlencoded",
+  //   "Content-Length: ".strlen($data)
+  // );
 
-  $header = array(
-    "Content-Type:application/x-www-form-urlencoded",
-    "Content-Length: ".strlen($data)
-  );
+  // $context = array(
+  //   "http" => array(
+  //     "method" => "POST",
+  //     "header" => $header,
+  //     "content" => $data,
+  //     'ignore_errors' => true,
+  //     'protocol_version' => '1.1',
+  //     'ssl' => array(
+  //       'verify_peer' => false,
+  //       'verify_peer_name' => false
+  //     )
+  //   )
+  // );
 
-  $context = array(
-    "http" => array(
-      "method" => "POST",
-      "header" => $header,
-      "content" => $data,
-      'ignore_errors' => true,
-      'protocol_version' => '1.1',
-      'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false
-      )
-    )
-  );
+  // $url = "https://notify-bot.line.me/oauth/token"; // API
 
-  $url = "https://notify-bot.line.me/oauth/token"; // API
-
-  $json = @file_get_contents($url, false, stream_context_create($context));
-  $arr = json_decode($json);
-  $accessToken = $arr->access_token;
-  session_start();
-  $_SESSION['accessToken'] = $accessToken; // アクセストークンをセッションに保持
+  // $json = @file_get_contents($url, false, stream_context_create($context));
+  // $arr = json_decode($json);
+  // $accessToken = $arr->access_token;
+  // session_start();
+  // $_SESSION['accessToken'] = $accessToken; // アクセストークンをセッションに保持
 ?>
 
 <!DOCTYPE html>
